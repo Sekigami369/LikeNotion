9b0033046d7612410895fdaf5101a9adc89331b0
sekigmai369 <skgmyum@gmail.com>
2023-08-03 07:06:43 +0900

    add Startup.cs

---
+++ Server/Models/SharedDocuHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace LikeNotion.Server.Models
+{
+    public class SharedDocuHub : Hub
+    {
+        public async Task UpdateDocuContent(string content)
+        {
+            await Clients.All.SendAsync("ReceiveDocumentContent", content);
+        }
+    }
+}
---
+++ Server/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace LikeNotion.Server
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // ...
+
+            ConfigureEndpoints(app);
+        }
+
+        private void ConfigureEndpoints(IApplicationBuilder app)
+        {
+            var endpointRouteBuilder = new EndpointRouteBuilder(app);
+            endpointRouteBuilder.MapHub<SharedDocumentHub>("/sharedDocumentHub");
+            endpointRouteBuilder.MapBlazorHub();
+            endpointRouteBuilder.MapFallbackToPage("/_Host");
+
+            var endpoints = endpointRouteBuilder.DataSources;
+            app.UseEndpoints(endpoints);
+        }
+
+    }
+}
