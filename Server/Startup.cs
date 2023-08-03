bde26dcf858156ef2cda9a83551fb8ac880e7880
sekigmai369 <skgmyum@gmail.com>
2023-08-03 22:13:05 +0900

    add endpoint

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Builder;
+using LikeNotion.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
 
 namespace LikeNotion.Server
 {
@@ -6,21 +11,29 @@ namespace LikeNotion.Server
     {
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // ...
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<SharedDocuHub>("/sharedDocumentHub");
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+            /*
+             *  var endpointRouteBuilder = new EndpointRouteBuilder(app);
+                endpointRouteBuilder.MapHub<SharedDocumentHub>("/sharedDocumentHub");
+                endpointRouteBuilder.MapBlazorHub();
+                endpointRouteBuilder.MapFallbackToPage("/_Host");
 
-            ConfigureEndpoints(app);
+                var endpoints = endpointRouteBuilder.DataSources;
+                app.UseEndpoints(endpoints);
+                ラムダ式で書かなかった場合の書き方
+                
+                */
+
         }
 
-        private void ConfigureEndpoints(IApplicationBuilder app)
+        public void ConfigureServices(IServiceCollection services)
         {
-            var endpointRouteBuilder = new EndpointRouteBuilder(app);
-            endpointRouteBuilder.MapHub<SharedDocumentHub>("/sharedDocumentHub");
-            endpointRouteBuilder.MapBlazorHub();
-            endpointRouteBuilder.MapFallbackToPage("/_Host");
-
-            var endpoints = endpointRouteBuilder.DataSources;
-            app.UseEndpoints(endpoints);
+            services.AddSignalR();
         }
-
     }
 }
